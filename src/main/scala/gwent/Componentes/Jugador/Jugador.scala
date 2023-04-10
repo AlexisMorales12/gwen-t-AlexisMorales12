c719b7f2d79ed8f408fb0d645c5099c9b723e7f5
unknown <alexis.morales@ug.uchile.cl>
2023-04-10 18:26:46 -0400

    quitadas algunas clases, se realizaron modificaciones a jugador y se añadieron test

--- src/main/scala/gwent/Componentes/Jugador/Jugador.scala
+++ src/main/scala/gwent/Componentes/Jugador/Jugador.scala
@@ -1,12 +1,24 @@
 package cl.uchile.dcc
 package gwent.Componentes.Jugador
 
-import gwent.Componentes.Cartas.Mazo
-import gwent.Componentes.Cartas.Mano
-class Jugador(nombre: String) {
+import gwent.Componentes.Cartas.CartaUnidad
+import gwent.Componentes.Acciones.Acciones
+class Jugador(nombre: String, seccion: String) {
   val Nombre: String = nombre
-  var Seccion: String = "Norte"
+  val Seccion: String = seccion
   var Gemas: Int = 25
-  var Mazo: Mazo = new Mazo
-  var Mano: Mano = new Mano
+  var Mazo: Int = 25
+  var Mano: Int = 1
+
+  def robar(obj:Jugador, numero: Int): Unit ={
+    if ((obj.Mazo-numero)>=0){
+      obj.Mazo -= numero
+      obj.Mano += numero
+    }
+  }
+  def jugar(obj: Jugador): Unit = {
+    if (obj.Mano>0){
+      obj.Mano -= 1
+    }
+  }
 }
